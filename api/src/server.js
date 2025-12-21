7f8c226c4a3af1d64bc3a3a05d22c52f088e884d
Eduard <nikinna86@gmail.com>
2025-12-21 12:55:52 +0200

    update

--- api/src/server.js
+++ api/src/server.js
@@ -13,7 +13,7 @@ import businessRouter from './routes/businessRoutes.js';
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 4000;
 
 app.use(express.json());
 app.use(
